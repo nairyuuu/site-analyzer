5faf8c49e40669e92702d50cc4cddcb148b0d9e8
Lê Long <133205729+nairyuuu@users.noreply.github.com>
2025-03-18 00:50:29 +0000

    Reformat template

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "scanResult") {
-        chrome.storage.local.set({ scanData: message.data });
-    }
-});
---
+++ src/background.js
@@ -0,0 +1,6 @@
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//     if (message.type === "scanResult") {
+//         chrome.storage.local.set({ scanData: message.data });
+//     }
+// });
+
