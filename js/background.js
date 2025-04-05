7b48da7c30b6dc1118f300199b614e3aa694385d
dmk1en <duongkien50@gmail.com>
2025-04-05 11:27:00 +0700

    add something

---
+++ js/background.js
@@ -0,0 +1,119 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+        // Remove this block to avoid redundant injection
+        // chrome.scripting.executeScript({
+        //     target: { tabId: tabId },
+        //     files: ['src/content.js']
+        // });
+    }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "scanResult") {
+        chrome.storage.local.set({ scanData: request.data }, () => {
+            console.log('Scan data stored in local storage:', request.data);
+        });
+    }
+});
+
+const anhlong = { 
+    technologies: [],
+    categories: [],
+
+    setTechnologies(technologies) {
+      anhlong.technologies = Object.entries(technologies).map(([name, data]) => ({ name, ...data }));
+    },
+
+    setCategories(categories) {
+        anhlong.categories = categories
+    }
+
+}
+
+const { setTechnologies, setCategories } = anhlong
+
+
+const Driver = {
+    async init() {
+        await Driver.loadTechnologies()
+    },
+
+    log(message, source = 'driver', type = 'log') {
+        // eslint-disable-next-line no-console
+        console[type](message)
+    },
+
+    error(error, source = 'driver') {
+        Driver.log(error, source, 'error')
+    },
+
+    async loadTechnologies() {
+        try {
+          const categories = await (
+            await fetch(chrome.runtime.getURL('categories.json'))
+          ).json()
+    
+          let technologies = {}
+    
+        //   for (const index of Array(27).keys()) {
+        //     const character = index ? String.fromCharCode(index + 96) : '_'
+    
+        //     technologies = {
+        //       ...technologies,
+        //       ...(await (
+        //         console.log(`js/technologies/${character}.json`),
+        //         await fetch(chrome.runtime.getURL(`js/technologies/${character}.json`))
+        //       ).json()),
+        //     }
+        //   }
+          technologies = {
+              ...technologies,
+              ...(await (
+              await fetch(chrome.runtime.getURL(`js/technologies/r.json`))
+              ).json())};
+    
+          // Object.keys(technologies).forEach((name) => {
+          // })
+    
+          setTechnologies(technologies)
+          setCategories(categories)
+        } catch (error) {
+          Driver.error(error)
+        }
+    },
+
+    onMessage({ source, func, args }, sender, callback) {
+        if (!func) {
+          return
+        }
+    
+        if (func !== 'log') {
+          Driver.log({ source, func, args })
+        }
+    
+        if (!Driver[func]) {
+          Driver.error(new Error(`Method does not exist: Driver.${func}`))
+    
+          return
+        }
+    
+        // eslint-disable-next-line no-async-promise-executor
+        new Promise(async (resolve) => {
+          resolve(Driver[func].call(Driver[func], ...(args || [])))
+        })
+          .then(callback)
+          .catch(Driver.error)
+    
+        return !!callback
+    },
+
+    getTechnologies() {
+        return anhlong.technologies
+    }
+}
+
+
+// Enable messaging between scripts
+chrome.runtime.onMessage.addListener(Driver.onMessage)
+
+Driver.init()
